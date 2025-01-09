7b73a395068dd40d883d0086c471d525c9379af9
PaulFreck <PaulFreck3@gmail.com>
2025-01-09 04:19:27 -0600

    increased the amount of memory used for the program from 4096 bytes to 110,000 bytes in order to facilitate storing more packets

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/paul/networking/p2/blink/simple_sniffer/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/paul/esp/v5.3.1/esp-idf/.git")
+set(GIT_DIR "/home/paul/networking/p2/blink/simple_sniffer/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
